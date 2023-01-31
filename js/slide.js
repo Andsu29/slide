beb9708eff6175e5db8dc794e3b7e2108f257dd4
Anderson Gomes Duarte Rodrigues <andersonrodrigues@glf.com.br>
2023-01-31 11:10:23 -0300

    final da navegaçao prev e nav

--- js/slide.js
+++ js/slide.js
@@ -8,6 +8,11 @@ export default class Slide {
       movement: 0,
     };
   }
+
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -27,6 +32,7 @@ export default class Slide {
       movetype = "touchmove";
     }
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transition(false);
   }
   onMove(event) {
     const pointerPosition =
@@ -40,6 +46,19 @@ export default class Slide {
     const movetype = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
+    console.log(this.dist.movement);
   }
 
   addSlideEvents() {
@@ -84,7 +103,20 @@ export default class Slide {
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
   }
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev);
+    }
+  }
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next);
+    }
+  }
+
   Init() {
+    this.transition(true);
     this.bindEvents();
     this.addSlideEvents();
     this.slidesConfig();
